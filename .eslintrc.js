9a60c540a757b093a644eef0f681a8fae044a594
Roni Hytonen <fnt5778@varian.com>
2021-08-28 20:00:05 +0300

    Get the frontend template from course git. Make it my own.

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+  'env': {
+    'node': true,
+    'browser': true,
+    'commonjs': true,
+    'es2021': true,
+    'jest': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'ecmaVersion': 12,
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'windows'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'eqeqeq': 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error', 'never'
+    ],
+    'arrow-spacing': [
+      'error', {'before': true, 'after': true}
+    ],
+    'no-console': 0,
+    "react/prop-types": "off"
+  }
+};
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# FullStackOpen-Blogs-Front
+# Full Stack Development Course
+Univ. Helsinki MOOC Full Stack Development course
+### Roni Hytönen - _roni.hytonen(at)aalto.fi_
+
+## Part 5
+### Tasks 
+
+Frontend code for Bloglist app.
+
+
+
